fa9b7ed9d0ddc4b8e1a8896fb3795aa8fdda4905
Krutarth Patel <krutarthzero@hotmail.com>
2019-01-10 08:45:17 +0530

    Update mycnn.py

--- mycnn.py
+++ mycnn.py
@@ -3,7 +3,7 @@ from torchvision import transforms
 from cnn import model_ft
 from PIL import Image
 
-real=Image.open('ant.jpg')
+real=Image.open('file_name.jpg')
 
 preprocess=transforms.Compose([
         transforms.Resize(224),
@@ -14,7 +14,6 @@ preprocess=transforms.Compose([
 
 Image=torch.FloatTensor(preprocess(real).unsqueeze_(0))
 model_ft.to('cpu')
-output=model_ft(Image)
-_,preds = torch.max(output, 1)
-print("the chances for it to be an ant is ",output[0][0])
-print("the chances for it to be a bee is ", output[0][1])
+output=model_ft(Image)#softmax values
+_,preds = torch.max(output, 1)#it will give output in 0/1 with the best choice of pridicted class(for 2 class)
+
